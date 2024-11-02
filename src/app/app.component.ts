5987b1f891eda6e41125d26080864656b54354a7
khalifa2003 <132180439+khalifa2003@users.noreply.github.com>
2024-11-03 00:08:56 +0200

    End Of Course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
+import { AsyncPipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {
   asapScheduler,
@@ -7,6 +8,7 @@ import {
   bindCallback,
   combineLatest,
   concat,
+  defer,
   delay,
   first,
   forkJoin,
@@ -19,19 +21,20 @@ import {
   of,
   pluck,
   share,
+  switchMapTo,
   take,
   takeUntil,
   takeWhile,
   tap,
 } from 'rxjs';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Rx.js-fundamentals';
+
 }
--- src/app/defer/defer.component.ts
+++ src/app/defer/defer.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { defer, fromEvent, tap, interval, switchMapTo, of, take } from 'rxjs';
 
 @Component({
   selector: 'app-defer',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './defer.component.html',
-  styleUrl: './defer.component.css'
+  styleUrl: './defer.component.css',
 })
 export class DeferComponent {
-
+  @ViewChild('button', { static: true })
+  buttonEL: ElementRef<HTMLButtonElement> = {} as any;
+  clicks$ = defer(() =>
+    fromEvent(this.buttonEL.nativeElement, 'click').pipe(
+      tap(() => console.log('clicked'))
+    )
+  );
+  obs1$ = interval(1000).pipe(
+    switchMapTo(of(Math.floor(Math.random() * 10))),
+    take(10)
+  );
+  obs2$ = interval(1000).pipe(
+    switchMapTo(defer(() => of(Math.floor(Math.random() * 10)))),
+    take(10)
+  );
+  ngOnInit(): void {
+    this.clicks$.subscribe();
+  }
 }
